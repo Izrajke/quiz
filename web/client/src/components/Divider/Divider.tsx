9d9e7c5875320c48baa912c3aac11e9dc93a3298
Ilya <IlyaAbdullinWork@yandex.ru>
2022-01-23 20:05:45 +0500

    создал компонент Divider

---
+++ web/client/src/components/Divider/Divider.tsx
@@ -0,0 +1,16 @@
+import { useMemo } from 'react';
+import type { FunctionComponent } from 'react';
+import clsx from 'clsx';
+
+import classes from './Divider.module.css';
+
+interface IDividerProps {
+  className?: string;
+}
+
+export const Divider: FunctionComponent<IDividerProps> = ({ className }) => {
+  const style = useMemo(() => clsx(classes.divider, className), [className]);
+  return <div className={style}></div>;
+};
+
+Divider.displayName = 'Divider';
