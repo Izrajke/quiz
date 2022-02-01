4a8ca16e2b87a5926347a867964f34aa1e95c361
Izrajke <e.nagornochyh@gmail.com>
2022-02-01 21:29:39 +0500

    добавлен случайный цвет для игрока

--- internal/domain/player.go
+++ internal/domain/player.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"math/rand"
+)
 
 const (
 	defaultPoints = 300
@@ -10,14 +13,24 @@ type Player struct {
 	Id     string `json:"id"`
 	Name   string `json:"name"`
 	Points int    `json:"points"`
+	Color  string `json:"color"`
 }
 
 // NewPlayer конструктор игрока
 func NewPlayer(name string) *Player {
+	// определение случайного цвета
+	colors := []string{
+		"player-1",
+		"player-2",
+		"player-3",
+	}
+	n := rand.Int() % len(colors)
+
 	return &Player{
 		Id:     uuid.New().String(),
 		Name:   name,
 		Points: defaultPoints,
+		Color:  colors[n],
 	}
 }
 
