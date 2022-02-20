6c82f9a1108905e5d8a8f0b28218f0560c63a854
Izrajke <e.nagornochyh@gmail.com>
2022-02-20 21:15:00 +0500

    исправлен cors

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -33,6 +33,14 @@ func main() {
 		id := uuid.New()
 		// enable cors
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		)
+		if r.Method == "OPTIONS" {
+			return
+		}
 		if r.Method == http.MethodPost {
 			id := struct {
 				ID uuid.UUID `json:"id"`
