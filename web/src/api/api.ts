fc4a5e33c6d48f4bfb347daf148c7215a9800839
Ilya <IlyaAbdullinWork@yandex.ru>
2022-02-22 20:13:56 +0500

    добавлена функция обращения к api

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,1 +1,29 @@
-export const BACKEND_URL = 'http://localhost:8080/';
+export const BACKEND_URL = 'http://localhost:8080';
+
+export interface ApiParams {
+  /** Информация о запросе */
+  input: RequestInfo;
+  /** Дополнения к запросу */
+  init?: RequestInit;
+}
+
+export const api = async ({ input, init }: ApiParams) => {
+  try {
+    const response = await fetch(input, {
+      ...init,
+      headers: {
+        ...init?.headers,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response['json']();
+
+      return data;
+    } else {
+      throw Error('error');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
