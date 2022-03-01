824e71e22c6b65c53b1d334bc11275f41800eb45
Ilya <IlyaAbdullinWork@yandex.ru>
2022-03-02 03:20:38 +0500

    добавлена функция создания анимации

--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -8,3 +8,29 @@ export const withDelay = <T>(
     args ? func(...args) : func();
   }, delay);
 };
+
+/** Создает тэг style и прокидывает туда анимацию
+ * @name - название анимации
+ * @context - css свойство
+ * @params - парамерты [от ---> до]
+ */
+export const createAnimationStyle = (
+  name: string,
+  context: string,
+  params: [string, string],
+) => {
+  const [from, to] = params;
+  const style = document.createElement('style');
+
+  style.innerHTML = `\
+  @-webkit-keyframes ${name} {\
+      0% {\
+        ${context}: ${from};\
+      }\
+      100% {\
+        ${context}: ${to};\
+    }\
+  }`;
+
+  return style;
+};
