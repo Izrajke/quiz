dd57598d6bc24a5c7a76c9c1d0a6f6780fcbeea0
Ilya <IlyaAbdullinWork@yandex.ru>
2022-04-30 19:04:53 +0500

    добавлена навигация на страницу Library и исправлен баг с позиционированием хедера при переключении страниц

--- web/src/app/Pages/Library/Library.tsx
+++ web/src/app/Pages/Library/Library.tsx
@@ -1,9 +1,18 @@
 import type { FunctionComponent } from 'react';
 
+import { Paper, Header } from 'components';
+
 import { observer } from 'mobx-react-lite';
 
+import classes from './Library.module.css';
+
 export const Library: FunctionComponent = observer(() => {
-  return null;
+  return (
+    <div className={classes.root}>
+      <Header />
+      <Paper className={classes.wrapper}></Paper>
+    </div>
+  );
 });
 
 Library.displayName = 'Library';
