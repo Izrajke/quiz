688deb477583a0a5caa362deee555d99ae53abb8
Izrajke <e.nagornochyh@gmail.com>
2022-05-01 19:07:17 +0500

    исправлен build

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"log"
+	"os"
 	"os/signal"
 	"quiz/internal/game"
 	httpserver "quiz/internal/http"
 	"quiz/internal/workerpool"
+	"strconv"
 	"sync"
 	"syscall"
 )
@@ -81,4 +84,45 @@ func newLogger() (*zap.Logger, error) {
 	opts.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
 	return opts.Build()
+}
+
+const (
+	appProd environment = "prod"
+)
+
+type environment string
+
+func (e environment) isProduction() bool {
+	return e != appProd
+}
+
+type config struct {
+	environment    environment
+	httpListenPort string
+	enablePprof    bool
+}
+
+func newConfig() (*config, error) {
+	env, found := os.LookupEnv("APP_ENV")
+	if !found {
+		return nil, errors.New("APP_ENV variable not found")
+	}
+	httpListenPort, found := os.LookupEnv("APP_HTTP_LISTEN")
+	if !found {
+		return nil, errors.New("APP_HTTP_LISTEN variable not found")
+	}
+	enablePprof, found := os.LookupEnv("APP_ENABLE_PPROF")
+	if !found {
+		return nil, errors.New("APP_ENABLE_PPROF variable not found")
+	}
+	enablePprofValue, err := strconv.ParseBool(enablePprof)
+	if err != nil {
+		return nil, errors.New("can't parse APP_ENABLE_PPROF")
+	}
+
+	return &config{
+		environment:    environment(env),
+		httpListenPort: httpListenPort,
+		enablePprof:    enablePprofValue,
+	}, nil
 }
