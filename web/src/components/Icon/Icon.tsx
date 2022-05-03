98ac0a538c66caba72fffe1d0c72fca216e2a6fa
Ilya <IlyaAbdullinWork@yandex.ru>
2022-05-03 05:43:40 +0500

    добавлены иконки starFilled и starOutline

--- web/src/components/Icon/Icon.tsx
+++ web/src/components/Icon/Icon.tsx
@@ -12,6 +12,8 @@ import { ReactComponent as Cog } from './svg/Cog.svg';
 import { ReactComponent as Pointer } from './svg/Pointer.svg';
 import { ReactComponent as Check } from './svg/Check.svg';
 import { ReactComponent as Info } from './svg/Info.svg';
+import { ReactComponent as StarFilled } from './svg/StarFilled.svg';
+import { ReactComponent as StarOutline } from './svg/StarOutline.svg';
 
 import classes from './Icon.module.css';
 
@@ -20,11 +22,13 @@ const icons = {
   pointer: Pointer,
   check: Check,
   info: Info,
+  starFilled: StarFilled,
+  starOutline: StarOutline,
 };
 
 export type IconTypes = keyof typeof icons;
 
-export type IconColors = 'white' | 'white-50';
+export type IconColors = 'white' | 'white-50' | 'gold' | 'gold-50';
 
 export interface IconProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
