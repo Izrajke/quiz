baf39183cc0dc442f1b866b693b11a45982e4286
Izrajke <e.nagornochyh@gmail.com>
2022-07-24 20:21:03 +0500

    добавлена реализация апи паков

--- internal/http/api/pack.go
+++ internal/http/api/pack.go
@@ -5,88 +5,13 @@ import (
 	"github.com/valyala/fasthttp"
 	"math"
 	"net/http"
+	"strconv"
 )
 
 type PackController struct{}
 
 func NewPackController() *PackController {
 	return &PackController{}
-}
-
-type pack struct {
-	Id       int     `json:"id"`
-	Title    string  `json:"title"`
-	Category string  `json:"category"`
-	Rating   float32 `json:"rating"`
-}
-
-var packs = []*pack{
-	{
-		Id:       1,
-		Title:    "Тестовое название 1",
-		Category: "История",
-		Rating:   1.3,
-	},
-	{
-		Id:       2,
-		Title:    "Тестовое название 2",
-		Category: "История",
-		Rating:   2.3,
-	},
-	{
-		Id:       3,
-		Title:    "Тестовое название 3",
-		Category: "История",
-		Rating:   3.3,
-	},
-	{
-		Id:       4,
-		Title:    "Тестовое название 4",
-		Category: "Литература",
-		Rating:   4.3,
-	},
-	{
-		Id:       5,
-		Title:    "Тестовое название 5",
-		Category: "Литература",
-		Rating:   1.3,
-	},
-	{
-		Id:       6,
-		Title:    "Тестовое название 6",
-		Category: "География",
-		Rating:   4.3,
-	},
-	{
-		Id:       7,
-		Title:    "Тестовое название 7",
-		Category: "География",
-		Rating:   2.3,
-	},
-	{
-		Id:       8,
-		Title:    "Тестовое название 8",
-		Category: "География",
-		Rating:   3.3,
-	},
-	{
-		Id:       9,
-		Title:    "Тестовое название 9",
-		Category: "Спорт",
-		Rating:   4.3,
-	},
-	{
-		Id:       10,
-		Title:    "Тестовое название 10",
-		Category: "Спорт",
-		Rating:   1.3,
-	},
-	{
-		Id:       11,
-		Title:    "Тестовое название 11",
-		Category: "Спорт",
-		Rating:   1.3,
-	},
 }
 
 func (s *PackController) HandleFilter(ctx *fasthttp.RequestCtx) {
@@ -101,22 +26,32 @@ func (s *PackController) HandleFilter(ctx *fasthttp.RequestCtx) {
 		return
 	}
 
+	var filterShortPacks []*shortPack
+	for _, currentPack := range fullPacks {
+		filterShortPacks = append(filterShortPacks, &shortPack{
+			Id:         currentPack.Id,
+			Title:      currentPack.Title,
+			CategoryId: currentPack.CategoryId,
+			Rating:     3.5,
+		})
+	}
+
 	page := 1
 	if request.Page != 0 {
 		page = request.Page
 	}
 	limit := 10
 	offset := (page - 1) * limit
-	total := int(math.Ceil(float64(len(packs)) / float64(limit)))
+	total := int(math.Ceil(float64(len(filterShortPacks)) / float64(limit)))
 
 	response := struct {
-		Content    []*pack `json:"content"`
+		Content    []*shortPack `json:"content"`
 		Pagination struct {
 			CurrentPage int `json:"currentPage"`
 			TotalPages  int `json:"totalPages"`
 		} `json:"pagination"`
 	}{
-		Content: packs[offset:],
+		Content: filterShortPacks[offset:],
 		Pagination: struct {
 			CurrentPage int `json:"currentPage"`
 			TotalPages  int `json:"totalPages"`
@@ -134,45 +69,112 @@ func (s *PackController) HandleFilter(ctx *fasthttp.RequestCtx) {
 }
 
 func (s *PackController) HandleCreate(ctx *fasthttp.RequestCtx) {
+	body := ctx.PostBody()
+
+	fullPackRequest := &fullPack{}
+	err := json.Unmarshal(body, &fullPackRequest)
+	if err != nil {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+	fullPackRequest.Id = len(fullPacks) + 1
+	fullPacks = append(fullPacks, fullPackRequest)
+
 	response := struct {
-		Field int `json:"test"`
-	}{Field: 1}
+		Success bool `json:"success"`
+	}{Success: true}
+
 	ctx.Response.Header.SetContentType("application/json")
 	ctx.Response.SetStatusCode(http.StatusOK)
-
 	jsonBody, _ := json.Marshal(response)
 	ctx.SetBody(jsonBody)
 }
 
 func (s *PackController) HandleUpdate(ctx *fasthttp.RequestCtx) {
-	response := struct {
-		Field int `json:"test"`
-	}{Field: 1}
+	body := ctx.PostBody()
+
+	packIdStr, ok := ctx.UserValue("id").(string)
+	if packIdStr == "" || !ok {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+	packIdInt, err := strconv.Atoi(packIdStr)
+	if err != nil {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+
+	fullPackRequest := &fullPack{}
+	err = json.Unmarshal(body, &fullPackRequest)
+	if err != nil {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+
+	for i, currentPack := range fullPacks {
+		if packIdInt == currentPack.Id {
+			fullPacks[i] = fullPackRequest
+		}
+	}
+
 	ctx.Response.Header.SetContentType("application/json")
 	ctx.Response.SetStatusCode(http.StatusOK)
-
-	jsonBody, _ := json.Marshal(response)
+	jsonBody, _ := json.Marshal(fullPackRequest)
 	ctx.SetBody(jsonBody)
 }
 
 func (s *PackController) HandleView(ctx *fasthttp.RequestCtx) {
-	response := struct {
-		Field int `json:"test"`
-	}{Field: 1}
+	packIdStr, ok := ctx.UserValue("id").(string)
+	if packIdStr == "" || !ok {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+	packIdInt, err := strconv.Atoi(packIdStr)
+	if err != nil {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+
+	var viewPack *fullPack
+	for _, currentPack := range fullPacks {
+		if packIdInt == currentPack.Id {
+			viewPack = currentPack
+			break
+		}
+	}
+
 	ctx.Response.Header.SetContentType("application/json")
 	ctx.Response.SetStatusCode(http.StatusOK)
-
-	jsonBody, _ := json.Marshal(response)
+	jsonBody, _ := json.Marshal(viewPack)
 	ctx.SetBody(jsonBody)
 }
 
 func (s *PackController) HandleDelete(ctx *fasthttp.RequestCtx) {
+	packIdStr, ok := ctx.UserValue("id").(string)
+	if packIdStr == "" || !ok {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+	packIdInt, err := strconv.Atoi(packIdStr)
+	if err != nil {
+		ctx.Error("bad request", fasthttp.StatusBadRequest)
+		return
+	}
+
+	var newPacks []*fullPack
+	for _, currentPack := range fullPacks {
+		if packIdInt != currentPack.Id {
+			newPacks = append(newPacks, currentPack)
+		}
+	}
+	fullPacks = newPacks
+
 	response := struct {
-		Field int `json:"test"`
-	}{Field: 1}
+		Success bool `json:"success"`
+	}{Success: true}
+
 	ctx.Response.Header.SetContentType("application/json")
 	ctx.Response.SetStatusCode(http.StatusOK)
-
 	jsonBody, _ := json.Marshal(response)
 	ctx.SetBody(jsonBody)
 }
---
+++ internal/http/api/pack_json.go
@@ -0,0 +1,35 @@
+package api
+
+type shortPack struct {
+	Id         int     `json:"id"`
+	Title      string  `json:"title"`
+	CategoryId int     `json:"categoryId"`
+	Rating     float32 `json:"rating"`
+}
+
+type rangeQuestion struct {
+	Title  string `json:"title"`
+	Answer int    `json:"answer"`
+}
+
+type multipleChoiceQuestionOption struct {
+	Option string `json:"option"`
+}
+
+type multipleChoiceQuestion struct {
+	Title   string                         `json:"title"`
+	Options []multipleChoiceQuestionOption `json:"options"`
+	Answer  int                            `json:"answer"`
+}
+
+type pack struct {
+	RangeQuestion           []rangeQuestion          `json:"rangeQuestion"`
+	MultipleChoiceQuestions []multipleChoiceQuestion `json:"multipleChoiceQuestions"`
+}
+
+type fullPack struct {
+	Id         int    `json:"id"`
+	CategoryId int    `json:"categoryId"`
+	Title      string `json:"title"`
+	Pack       pack   `json:"pack"`
+}
